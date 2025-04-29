b6a6dc39d445dbc3958f09bf7de939eafacd719b
mateo <mateoghiano5@gmail.com>
2025-04-29 18:35:50 -0300

    contacts validation

--- contacts-app-laravel/database/migrations/2025_04_28_175921_create_contacts_table.php
+++ contacts-app-laravel/database/migrations/2025_04_28_175921_create_contacts_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('phone_number');
+            $table->string('email');
+            $table->tinyInteger('age', unsigned: true); // para un dato pequeño 
             $table->timestamps();
         });
     }
--- contacts-app-laravel/resources/views/contact.blade.php
+++ contacts-app-laravel/resources/views/contact.blade.php
@@ -14,17 +14,58 @@
                 <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
 
                 <div class="col-md-6">
-                  <input id="name" type="text" class="form-control" name="name" required autocomplete="name"
-                    autofocus>
+                  <input value="{{ old('name') }}" id="name" type="text"
+                    class="form-control @error('name') is-invalid @enderror" name="name" autocomplete="name" autofocus>
+                  @error('name')
+                    <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                    </span>
+                  @enderror
                 </div>
 
                 <div class="row mb-3">
                   <label for="phone_number" class="col-md-4 col-form-label text-md-end">Phone Number</label>
 
                   <div class="col-md-6">
-                    <input id="phone_number" type="tel" class="form-control" name="phone_number" required>
+                    <input value="{{ old('phone_number') }}" id="phone_number" type="tel"
+                      class="form-control @error('phone_number') is-invalid @enderror" name="phone_number">
+                    @error('phone_number')
+                      <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                      </span>
+                    @enderror
                   </div>
                 </div>
+
+                <div class="row mb-3">
+                  <label for="email" class="col-md-4 col-form-label text-md-end">Email</label>
+
+                  <div class="col-md-6">
+                    <input value="{{ old('email') }}" id="email" type="text"
+                      class="form-control @error('email') is-invalid @enderror" name="email">
+                    @error('email')
+                      <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                      </span>
+                    @enderror
+                  </div>
+                </div>\
+
+
+                <div class="row mb-3">
+                  <label for="age" class="col-md-4 col-form-label text-md-end">Age</label>
+
+                  <div class="col-md-6">
+                    <input value="{{ old('age') }}" id="age" type="text"
+                      class="form-control @error('age') is-invalid @enderror" name="age">
+                    @error('age')
+                      <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                      </span>
+                    @enderror
+                  </div>
+                </div>
+
 
                 <div class="row mb-0">
                   <div class="col-md-6 offset-md-4">
